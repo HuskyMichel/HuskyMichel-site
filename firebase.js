20cb3d206baedb19f087fbbd94bf874718ee4401
HuskyMichel <michelturain4@gmail.com>
2026-07-12 20:40:26 +0200

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,42 @@
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+
+import { 
+getAuth 
+} from 
+"https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+
+import { 
+getFirestore 
+} from 
+"https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+
+
+const firebaseConfig = {
+
+apiKey: "AIzaSyA_4v5IjbJBT3R6dyKAtGmsqy-EA_3a73U",
+
+authDomain: "huskymichel2.firebaseapp.com",
+
+projectId: "huskymichel2",
+
+storageBucket: "huskymichel2.firebasestorage.app",
+
+messagingSenderId: "803041103978",
+
+appId: "1:803041103978:web:0415d72f91e30735a47a13"
+
+};
+
+
+
+const app = initializeApp(firebaseConfig);
+
+
+export const auth = getAuth(app);
+
+
+export const db = getFirestore(app);
